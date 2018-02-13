4a9b9ea08646bd3085952766c2dd0588799c7c01
Andres Velez <avandrews669@gmail.com>
2018-02-12 22:31:21 -0500

    Login

---
+++ Lands_1/Lands_1/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace Lands_1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Xamarin.Forms;
+    using views;
+    public partial class App : Application
+	{
+
+        #region constructors
+        public App ()
+		{
+			InitializeComponent();
+
+            MainPage = new LoginPage();
+		}
+        #endregion
+
+        #region Methods
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        } 
+        #endregion
+    }
+}
